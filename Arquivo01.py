62046ad6a61a10d5546af3aa4d1dd70c93be1a7f
will <wilkisonsouza167@gmail.com>
2025-04-01 08:50:38 -0300

    Primeira tarefa do dia 01/02/25 concluida

---
+++ Arquivo01.py
@@ -0,0 +1,5 @@
+nome = input ("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+salario = float(input("Digite seu salario:"))
+
+print (f"Olá {nome},sua idade é {idade} anos e seu salario é {salario}")
