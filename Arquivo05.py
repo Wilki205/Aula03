318f3236984354ce1693b75c02fdf73a727d7c90
will <wilkisonsouza167@gmail.com>
2025-04-01 11:23:27 -0300

    subindo atividade 5 com erro de indenteação, precisa corrigir menó

--- Arquivo05.py
+++ Arquivo05.py
@@ -7,4 +7,7 @@ media = (n1+n2+n3)/3
 if media >= 7:
     print(f"ALUNO APROVADO APROVADO MEDIA {media:.2f}")
 else:
+    if media >= 4:
     print(f"ALUNO REPROVADO MEDIA {media:.2f}")
+    else:
+    print(f"ALUNO DE RECUPERAÇÃO MEDIA {media:.2f}")
