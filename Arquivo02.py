f1c01deffbc105bdf4c0252cf79f7bed53d9088c
will <wilkisonsouza167@gmail.com>
2025-04-01 09:46:53 -0300

    segunda atividade entrege 01/04/25

---
+++ Arquivo02.py
@@ -0,0 +1,4 @@
+N1 = int(input("digite seu numero: "))
+N2 = int(input("digite seu numero: "))
+
+print(f" A soma de de n1 e n2 é {N1+N2}, a multiplicação é {N1*N2}, a subitração é {N1-N2} e a divisão é {N1/N2:.2f}")
