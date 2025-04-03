efe88e7883f67d3a1a556df1898b643f0b84cfa7
will <wilkisonsouza167@gmail.com>
2025-04-03 09:14:00 -0300

    atualização do codigo para aceitar mais de uma condição, quinta feira sofrida menó

--- Arquivo07.py
+++ Arquivo07.py
@@ -1,14 +1,14 @@
-tipo = input("digite G para gasolina e E para etanol: ")
+tipo = input("digite G  para gasolina e E  para etanol: ")
 G = 5.80
 E = 4.90
 litros = int(input("digite qtd de litros: "))
-preco = G*litros
-preco2 = E*litros
-if tipo =="G":
+preco = 0
+if tipo =="G" or tipo == "g":
      preco = litros*5.80
+elif tipo == "E" or tipo == "e":
+     preco = litros*4.90
 else:
-     preco2 = litros*4.90
-
+    print ( "tipo de combustivel invalido")
 print (f"voce pagou R${preco:.2f} de combustivel")
 
 
