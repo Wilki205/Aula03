66866aebac0a3f9407902b8bce178a2710988f4b
will <wilkisonsouza167@gmail.com>
2025-04-08 10:58:03 -0300

    atividade 9, sussa e agr é otimizar

---
+++ Arquivo09.py
@@ -0,0 +1,30 @@
+n = int(input("Digite um numero de 1 a 12: "))
+
+if 1 <= n <= 12:
+
+    if n ==1:
+        print ("Janeiro")
+    elif n ==2:
+        print ("Fevereiro")
+    elif n ==3:
+        print ("Março")
+    elif n ==4:
+        print ("Abril")
+    elif n ==5:
+        print ("Maio")
+    elif n ==6:
+        print ("Junho")
+    elif n ==7:
+        print ("Julho")
+    elif n ==8:
+        print ("Agosto")
+    elif n ==9:
+        print ("Setembro")
+    elif n ==10:
+        print ("Outubro")
+    elif n ==11:
+        print ("Novembro")
+    elif n ==12:
+        print ("Dezembro")
+else:
+    print ("NUMERO INVALIDO!")
